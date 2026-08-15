5ff8c982188db234569f36ba4be6e14d8d925356
lilougagou-prog <lilougagou@gmail.com>
2026-08-15 23:20:24 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,19 @@
-const CACHE="arcanes-v1";
+const CACHE="arcanes-v2";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("install",e=>e.waitUntil(
+  caches.open(CACHE)
+    .then(c=>c.addAll(ASSETS))
+    .then(()=>self.skipWaiting())
+));
+
+self.addEventListener("activate",e=>e.waitUntil(
+  caches.keys()
+    .then(keys=>Promise.all( keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    )
+    .then(()=>self.clients.claim())
+));
+
+self.addEventListener("fetch",e=>e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+));
